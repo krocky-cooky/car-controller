6086dd21663dda5b26bc6c0dd5e2f38953f00f63
Tomoki-Japan <tomoki.kurikuri.1122@gmail.com>
2021-01-02 17:48:08 +0900

    目標コース修正を取りやめ

--- src/beginner_tutorial/src/horizontal_diviation.cpp
+++ src/beginner_tutorial/src/horizontal_diviation.cpp
@@ -20,21 +20,10 @@ void view_point_x_y_theta_array_callback(const beginner_tutorial::carPosition::C
 void target_x_y_array_callback(const std_msgs::Float32MultiArray::ConstPtr& target_x_y_array){
   float delta_x;
   float delta_y;
-  float temp_horizontal_diviation;
 
-  //目標点列データのなかで前方注視点との距離が最小のものを探す
-  horizontal_diviation.data = 1000;
-  int i;
-  int iter;
-  iter = 500;
-  for (i=0; i<iter; i++) {
-  delta_x = buf_view_point_x_y_theta_array[0] - target_x_y_array->data.at(2*i);
-  delta_y = buf_view_point_x_y_theta_array[1] - target_x_y_array->data.at(2*i+1);
-  temp_horizontal_diviation = sqrtf(powf(delta_x, 2.0f) + powf(delta_y, 2.0f));
-  if (temp_horizontal_diviation < horizontal_diviation.data){
-    horizontal_diviation.data = temp_horizontal_diviation;
-  } 
-  }
+  delta_x = buf_view_point_x_y_theta_array[0] - target_x_y_array->data.at(0);
+  delta_y = buf_view_point_x_y_theta_array[1] - target_x_y_array->data.at(1);
+  horizontal_diviation.data = sqrtf(powf(delta_x, 2.0f) + powf(delta_y, 2.0f));
 
   ROS_INFO("horizontal_diviation: [%.1f]", horizontal_diviation.data);
   horizontal_diviation_pub.publish(horizontal_diviation);
--- src/beginner_tutorial/src/target_x_y_array.cpp
+++ src/beginner_tutorial/src/target_x_y_array.cpp
@@ -6,38 +6,6 @@
 ros::Publisher target_x_y_array_pub;
 std_msgs::Float32MultiArray target_x_y_array;
 
-float buf_x_y_theta_array[3];
-int count = 0;
-
-void x_y_theta_array_callback(const beginner_tutorial::carPosition::ConstPtr& x_y_theta_array){
-
-  buf_x_y_theta_array[0] = x_y_theta_array->x;
-  buf_x_y_theta_array[1] = x_y_theta_array->y;
-  buf_x_y_theta_array[2] = x_y_theta_array->yaw;
-}
-
-void timer_callback(const ros::TimerEvent& e){
-    //目標コースy=1+sin(6.28 * x / 166.6)
-    //点列間隔は0.2mで、100m先まで返す。つまり点列は500個
-    target_x_y_array.data[0] = 8.33f*0.025*(float)count;
-    target_x_y_array.data[1] = 1.0;
-
-    int i;
-    int iter;
-    float x;
-    float space = 0.2;
-    iter = 500;
-    for (i=0; i<iter; i++) {
-    x = buf_x_y_theta_array[0] + space*i;
-    target_x_y_array.data[2*i] = x;
-    target_x_y_array.data[2*i+1] = 1+sin(6.28 * x / 166.6);
-    ROS_INFO("x: %.1f  y: %.1f", target_x_y_array.data[2*i], target_x_y_array.data[2*i+1]);
-    }
-    
-    target_x_y_array_pub.publish(target_x_y_array);
-}
-
-/*
 int count = 0;
 
 void timer_callback(const ros::TimerEvent& e){
@@ -49,7 +17,6 @@ void timer_callback(const ros::TimerEvent& e){
     ROS_INFO("x: %.1f  y: %.1f", target_x_y_array.data[0], target_x_y_array.data[1]);
     target_x_y_array_pub.publish(target_x_y_array);
 }
-*/
 
 int main(int argc, char **argv){
 
