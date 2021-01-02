a80f7b152e895b0334ee11b1ec1b474ca3cc8cc1
Tomoki-Japan <tomoki.kurikuri.1122@gmail.com>
2021-01-02 13:31:08 +0900

    目標コースを点から点列に変更

--- src/beginner_tutorial/src/target_x_y_array.cpp
+++ src/beginner_tutorial/src/target_x_y_array.cpp
@@ -1,12 +1,44 @@
 #include "ros/ros.h"
 #include <std_msgs/Float32MultiArray.h>
 #include <math.h>
+#include "beginner_tutorial/carPosition.h"
 
 
 ros::Publisher target_x_y_array_pub;
 std_msgs::Float32MultiArray target_x_y_array;
-int count = 0;
 
+float buf_x_y_theta_array[3];
+
+void x_y_theta_array_callback(const beginner_tutorial::carPosition::ConstPtr& x_y_theta_array){
+
+  buf_x_y_theta_array[0] = x_y_theta_arrray->x;
+  buf_x_y_theta_array[1] = x_y_theta_array->y;
+  buf_x_y_theta_array[2] = x_y_theta_array->yaw;
+}
+
+void timer_callback(const ros::TimerEvent& e){
+    //目標コースy=1+sin(6.28 * x / 166.6)
+    //点列間隔は0.2mで、100m先まで返す。つまり点列は500個
+    target_x_y_array.data[0] = 8.33f*0.025*(float)count;
+    target_x_y_array.data[1] = 1.0;
+
+    int i;
+    int iter;
+    float x;
+    space = 0.2
+    iter = 500
+    for (i=0; i<iter; i++) {
+    x = buf_x_y_theta_array[0] + space*i
+    target_x_y_array.data[2*i] = x;
+    target_x_y_array.data[2*i+1] = 1+sin(6.28 * x / 166.6);
+    ROS_INFO("x: %.1f  y: %.1f", target_x_y_array.data[2*i], target_x_y_array.data[2*i+1]);
+    }
+    
+    target_x_y_array_pub.publish(target_x_y_array);
+}
+
+/*
+int count = 0;
 void timer_callback(const ros::TimerEvent& e){
     //目標コースとしてx軸を想定
     ++count;
@@ -16,6 +48,7 @@ void timer_callback(const ros::TimerEvent& e){
     ROS_INFO("x: %.1f  y: %.1f", target_x_y_array.data[0], target_x_y_array.data[1]);
     target_x_y_array_pub.publish(target_x_y_array);
 }
+*/
 
 int main(int argc, char **argv){
 
@@ -23,9 +56,9 @@ int main(int argc, char **argv){
 
   ros::NodeHandle n;
   target_x_y_array.data.resize(3);
-  target_x_y_array_pub = n.advertise<std_msgs::Float32MultiArray>("target_x_y_array", 2);
-
-  ros::Timer timer = n.createTimer(ros::Duration(0.025), timer_callback);
+  target_x_y_array_pub = n.advertise<std_msgs::Float32MultiArray>("target_x_y_array", 1000);
+  ros::Subscriber x_y_theta_array_sub = n.subscribe("car_position", 1000, x_y_theta_array_callback);
+  ros::Timer timer = n.createTimer(ros::Duration(10), timer_callback);
   ros::spin();
 
   return 0;
