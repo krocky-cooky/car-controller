76da51a4eeb52103c978a9dd4060ac60fdaa1073
Tomoki-Japan <tomoki.kurikuri.1122@gmail.com>
2021-01-02 18:58:51 +0900

    horizontal_diviationがview_point_x_y_arrayを受け取るたびにpublishされるように修正

--- src/beginner_tutorial/src/horizontal_diviation.cpp
+++ src/beginner_tutorial/src/horizontal_diviation.cpp
@@ -8,8 +8,9 @@ ros::Publisher horizontal_diviation_pub;
 std_msgs::Float32 horizontal_diviation;
 
 float buf_view_point_x_y_theta_array[3];
+float buf_target_x_y_array[1000];
 
-
+/*
 void view_point_x_y_theta_array_callback(const beginner_tutorial::carPosition::ConstPtr& veiw_point_x_y_theta_array){
 
   buf_view_point_x_y_theta_array[0] = veiw_point_x_y_theta_array->x;
@@ -39,6 +40,40 @@ void target_x_y_array_callback(const std_msgs::Float32MultiArray::ConstPtr& targ
   ROS_INFO("horizontal_diviation: [%.1f]", horizontal_diviation.data);
   horizontal_diviation_pub.publish(horizontal_diviation);
 }
+*/
+void target_x_y_array_callback(const std_msgs::Float32MultiArray::ConstPtr& target_x_y_array){
+  int i;
+  int iter;
+  iter = 500;
+  for (i=0; i<iter; i++) {
+  buf_target_x_y_array[2*i] = target_x_y_array->data.at(2*i);
+  buf_target_x_y_array[2*i+1] = target_x_y_array->data.at(2*i);
+  }
+}
+
+void view_point_x_y_theta_array_callback(const beginner_tutorial::carPosition::ConstPtr& veiw_point_x_y_theta_array){
+  float delta_x;
+  float delta_y;
+  float temp_horizontal_diviation;
+
+  //目標点列データのなかで前方注視点との距離が最小のものを探す
+  horizontal_diviation.data = 1000;
+  int i;
+  int iter;
+  iter = 500;
+  for (i=0; i<iter; i++) {
+  delta_x = veiw_point_x_y_theta_array->x - buf_target_x_y_array->data.at(2*i);
+  delta_y = veiw_point_x_y_theta_array->y - buf_target_x_y_array->data.at(2*i+1);
+  temp_horizontal_diviation = sqrtf(powf(delta_x, 2.0f) + powf(delta_y, 2.0f));
+  if (temp_horizontal_diviation < horizontal_diviation.data){
+    horizontal_diviation.data = temp_horizontal_diviation;
+  } 
+  }
+
+  ROS_INFO("horizontal_diviation: [%.1f]", horizontal_diviation.data);
+  horizontal_diviation_pub.publish(horizontal_diviation);
+}
+
 
 int main(int argc, char **argv){
 
--- src/beginner_tutorial/src/view_point_x_y_theta_array.cpp
+++ src/beginner_tutorial/src/view_point_x_y_theta_array.cpp
@@ -28,7 +28,7 @@ void x_y_theta_array_callback(const beginner_tutorial::carPosition::ConstPtr& x_
 
 int main(int argc, char **argv){
 
-  ros::init(argc, argv, "forward_veiw_point");
+  ros::init(argc, argv, "veiw_point_x_y_theta_array");
 
   ros::NodeHandle n;
   veiw_point_x_y_theta_array_pub = n.advertise<beginner_tutorial::carPosition>("view_point_x_y_theta_array",1000);
